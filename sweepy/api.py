378c9819a04e6b3ed9e511c29c868dfb01287ca6
Rory Devitt <rorytdevitt@gmail.com>
2025-04-09 18:00:53 +0100

    Fix API and update dependencies

--- sweepy/api.py
+++ sweepy/api.py
@@ -1,9 +1,21 @@
-from fastapi import APIRouter, HTTPException
+import os
+import dotenv
+from fastapi import FastAPI, HTTPException
 
+from sweepy.integrations.betfair import BetfairClient
 from sweepy.models import SweepstakesRequest, SweepstakesResponse
 from sweepy import generate_sweepstakes
 
-app = APIRouter()
+app = FastAPI()
+
+dotenv.load_dotenv()
+
+
+betfair_client = BetfairClient(
+    username=os.getenv("BETFAIR_USERNAME"),
+    password=os.getenv("BETFAIR_PASSWORD"),
+    app_key=os.getenv("BETFAIR_APP_KEY"),
+)
 
 
 @app.post("/sweepstakes")
@@ -11,13 +23,9 @@ def create_sweepstakes(request: SweepstakesRequest) -> SweepstakesResponse:
     """
     Create a new sweepstake.
     """
-    # Validate the request
-    if not request.participants:
-        raise HTTPException(status_code=400, detail="Participants list cannot be empty")
-
     # Create a new sweepstake
     try:
-        response = generate_sweepstakes.generate_sweepstakes(request)
+        response = generate_sweepstakes.generate_sweepstakes(betfair_client, request)
     except Exception as e:
         raise HTTPException(
             status_code=500, detail=f"Error creating sweepstake: {str(e)}"
