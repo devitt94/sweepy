f65228af47661d846b754bf582bb57060d1a98d5
Rory Devitt <rorytdevitt@gmail.com>
2025-06-26 08:31:08 +0100

    Use 3 participants by default

--- sweepy-ui/src/components/CreateForm.jsx
+++ sweepy-ui/src/components/CreateForm.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
+const defaultFormData = {
+  market_id: "",
+  method: "tiered",
+  name: "",
+  participants: ["", "", ""],
+};
+
 function CreateFrom({handleSubmitSuccess}) {
-  const [formData, setFormData] = useState({
-    market_id: "",
-    method: "tiered",
-    name: "",
-    participants: [""],
-  });
+  const [formData, setFormData] = useState({...defaultFormData});
 
   const [error, setError] = useState(null);
 
@@ -38,12 +40,7 @@ function CreateFrom({handleSubmitSuccess}) {
         participants: sortedParticipants, // Use sorted participants
       });
 
-      setFormData({
-        market_id: "",
-        method: "tiered",
-        name: "",
-        participants: [""],
-      });
+      setFormData({ ...defaultFormData }); // Reset form data
       
     } catch (err) {
       console.error("Error creating sweepstake:", err.stack);
